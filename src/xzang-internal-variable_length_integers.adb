86698f283eccc52f563be4156911ae4e3aec7fe6
Pavel Zhukov <landgraf@fedoraproject.org>
2013-12-09 10:46:17 +0100

    Improve variable length integer decoder.

--- src/xzang-internal-variable_length_integers.adb
+++ src/xzang-internal-variable_length_integers.adb
@@ -2,16 +2,10 @@ package body xzang.internal.variable_length_integers is
 
    function Decode (Source : in byte_array) return Unsigned_64 is
       Result : Unsigned_64 := 0;
-      Modvalue : constant := 2**8;
-      Tmp : Unsigned_64;
    begin
-      if Source'Length > 1 then
-         for I in 1..Source'Length - 1 loop
-            warning("Shifting");
-            Result := Shift_Left(Result, 7) + (abs (Unsigned_64'Val(Source(I) - 128)));
-         end loop;
-      end if;
-      Tmp := Unsigned_64'val(Source(1)) ;
+       for I in 1..Source'Length - 1 loop
+           Result := Shift_Left(Result + (Unsigned_64'Val(Source(I)) and 16#7F#), 7);
+       end loop;
       Result := Result + Unsigned_64'val(Source(1));
       return Result;
    end Decode;
